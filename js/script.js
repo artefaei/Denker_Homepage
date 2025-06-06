8641bef8303bc70426bfa5463b1ee6f195f291d6
Sage <smermaidx@gmail.com>
2025-06-06 13:55:54 +0200

    June Start 2025

--- js/script.js
+++ js/script.js
@@ -425,3 +425,21 @@ window.hidePortraitModal = function() {
   }
   if (ziel) wechsel();
 })();
+
+var lastScrollTop = 0;
+$(window).scroll(function(event){
+  var st = $(this).scrollTop();
+  if (st > lastScrollTop){
+    if (!$('body').hasClass('down')) {
+      $('body').addClass('down');
+    }
+   } else {
+     $('body').removeClass('down');
+   }
+
+   lastScrollTop = st;
+
+   if ($(this).scrollTop() <= 0) {
+     $('body').removeClass('down');
+   };
+});
